40b372384b94f580edd1b744b990442a4843898f
amandabriena <amanda.briena@gmail.com>
2023-04-12 23:32:11 -0300

    Start design for the User Rest API
    - Create User Bean and Dao Service
    - Implemented Get and Post methods to get user, user list and create a new user
    - Implemented Exception Handler 404: User not found

---
+++ src/main/java/com/hellospring/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,37 @@
+package com.hellospring.restfulwebservices.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+
+	private static List<User> users = new ArrayList<>();
+
+	private static int usersCount = 0;
+
+	static {
+		users.add(new User(++usersCount, "Amanda", LocalDate.now().minusYears(24)));
+		users.add(new User(++usersCount, "Teste", LocalDate.now().minusYears(18)));
+		users.add(new User(++usersCount, "Feeling 22", LocalDate.now().minusYears(22)));
+	}
+
+	public List<User> findAll() {
+		return users;
+	}
+
+	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		return users.stream().filter(predicate).findFirst().orElse(null);
+	}
+
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
+}
---
+++ src/main/java/com/hellospring/restfulwebservices/user/UserResource.java
@@ -0,0 +1,43 @@
+package com.hellospring.restfulwebservices.user;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class UserResource {
+	private UserDaoService service;
+
+	public UserResource(UserDaoService service) {
+		this.service = service;
+	}
+
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) throws UserNotFoundException {
+		User user = service.findOne(id);
+		if (user == null) {
+			throw new UserNotFoundException("id: " + id);
+		}
+		return user;
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		URI locationUri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(service.save(user).getId()).toUri();
+		return ResponseEntity.created(locationUri).build();
+	}
+
+}
