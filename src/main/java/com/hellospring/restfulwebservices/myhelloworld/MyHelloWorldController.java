9e4c027b294b3d30903f698a4e50d619af30386b
amandabriena <amanda.briena@gmail.com>
2023-04-17 23:19:15 -0300

    Add Content Negotiation
    -Content Negotiation: Consumer tells the REST API provider what kind of representation is required (e.g: to accept header as JSON or XML, accept language header)
    - To request a XML response, the API consumer only have to add a header with name Accept and the value application/xml
    - Internationalization: To request a localized response, the API consumer only have to add a header with name Accept and a value related to the country (en, fr, br..)
    - Message Source added with message files to internationalize the api return

--- src/main/java/com/hellospring/restfulwebservices/myhelloworld/MyHelloWorldController.java
+++ src/main/java/com/hellospring/restfulwebservices/myhelloworld/MyHelloWorldController.java
@@ -1,11 +1,23 @@
 package com.hellospring.restfulwebservices.myhelloworld;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class MyHelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public MyHelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	@GetMapping(path ="/my-hello-world")
 	public String helloWorld() {
@@ -20,6 +32,12 @@ public class MyHelloWorldController {
 	@GetMapping(path ="/my-hello-world/path-variable/{name}")
 	public MyHelloWorldBean helloWorldBeanPathParameters(@PathVariable String name) {
 		return new MyHelloWorldBean(String.format("Hello world %s !", name));
+	}
+	
+	@GetMapping(path ="/my-hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("hello.world.message", null, "Default Message", locale);
 	}
 
 }
